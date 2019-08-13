f2ef4d5fd86a4b47460a28348ea58ef05eee657e
Benjamin Munk <benjamin@karstenmunk.dk>
2019-08-13 23:10:10 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Benjamin on 13/08/2019.
+//  Copyright © 2019 Benjamin Munk. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
